b6dfcf563555bf62cc801e29081c6c73441de587
Simon <s.dahan@leshabitues.fr>
2022-12-12 14:52:53 +0100

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Simon Dahan on 12/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
